6ad0cde7f850941f41f542959432c54d1b06567e
David Chen <chendahui007@gmail.com>
2017-05-09 16:05:16 +0800

    syncer: split label into two (#130)
    * syncer: split label into two
    
    grafana query:
    
    ```
    syncer_gtid{node="master"} - on(instance,job,server_uuid)
    syncer_gtid{node="syncer"}
    ```

--- syncer/syncer.go
+++ syncer/syncer.go
@@ -640,8 +640,8 @@ func (s *Syncer) run() (err error) {
 			}
 		}
 
-		binlogPos.WithLabelValues("syncer_binlog_pos").Set(float64(e.Header.LogPos))
-		binlogFile.WithLabelValues("syncer_binlog_file").Set(getBinlogIndex(s.meta.Pos().Name))
+		binlogPos.WithLabelValues("syncer").Set(float64(e.Header.LogPos))
+		binlogFile.WithLabelValues("syncer").Set(getBinlogIndex(s.meta.Pos().Name))
 
 		switch ev := e.Event.(type) {
 		case *replication.RotateEvent:
@@ -796,9 +796,7 @@ func (s *Syncer) run() (err error) {
 			gtid = fmt.Sprintf("%s:1-%d", u.String(), ev.GNO)
 			log.Debugf("gtid infomation: binlog %v, gtid %s", pos, gtid)
 
-			label := fmt.Sprintf("syncer_binlog_gtid_%s", u.String())
-			log.Debugf("gauge syncer gtid label:%s, gno:%d", label, ev.GNO)
-			binlogGTID.WithLabelValues(label).Set(float64(ev.GNO))
+			binlogGTID.WithLabelValues("syncer", u.String()).Set(float64(ev.GNO))
 		}
 	}
 }
@@ -886,8 +884,8 @@ func (s *Syncer) printStatus() {
 					}
 					log.Errorf("[syncer] get master status error %s", err.Error())
 				} else {
-					binlogPos.WithLabelValues("master_binlog_pos").Set(float64(masterPos.Pos))
-					binlogFile.WithLabelValues("master_binlog_file").Set(getBinlogIndex(masterPos.Name))
+					binlogPos.WithLabelValues("master").Set(float64(masterPos.Pos))
+					binlogFile.WithLabelValues("master").Set(getBinlogIndex(masterPos.Name))
 					masterGTIDGauge(masterGTIDs)
 				}
 			}
